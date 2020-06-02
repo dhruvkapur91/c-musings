63d145f005abf0ec243034107d910dd0b1b97d21
Dhruv Kapur <dhruvkapur@Dhruvs-MacBook-Pro.local>
2020-06-02 20:52:45 +0530

    Update readme with memory command and printing of process ids

--- fill-list/fill_without_print.cpp
+++ fill-list/fill_without_print.cpp
@@ -1,12 +1,22 @@
 
 #include <iostream> 
 #include <list> 
+#include <stdio.h>
+#include <unistd.h>
 using namespace std; 
   
 int main() 
 { 
+    int p_id,p_pid;
+     
+    p_id=getpid();  /*process id*/
+    p_pid=getpid(); /*parent process id*/
+     
+    printf("Process ID: %d\n",p_id);
+    printf("Parent Process ID: %d\n",p_pid);
+
     unsigned long counter = 0;
-    list <long> someList, gqlist2; 
+    list <long> someList;
     while(true) {
         someList.push_back(counter);
         counter++;
